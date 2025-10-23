d8efc1e239e90ebbf7ab764892b63e7276ecce5f
gitwatch <gitwatch@dsm.local>
2025-10-23 20:20:34 +0000

    Scripted auto-commit on change (2025-10-23 20:20:34) by gitwatch.sh

--- gitwatch/docker-entrypoint.sh
+++ gitwatch/docker-entrypoint.sh
@@ -3,4 +3,6 @@ set -euo pipefail
 
 /usr/local/bin/gitwatch-pre-flight.sh || exit 1
 
-exec /usr/local/bin/gitwatch "$@" 1>&1 2>&1
+exec /usr/local/bin/gitwatch "$@" 1>&1 2>&1 &
+
+git diff --name-only | xargs -r -I{} touch "{}"
